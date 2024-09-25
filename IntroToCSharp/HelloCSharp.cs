b48962c35561e9e99c7e0205f7ff9bf02beb705d
razadan1 <danmoleomomayowa@gmail.com>
2024-09-25 12:55:32 +0100

    Add project files.

---
+++ IntroToCSharp/HelloCSharp.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace IntroToCSharp
+{
+    class HelloCSharp
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, C#!");
+            Console.WriteLine("Good Day!");
+            Console.Write("FirstName: Ahmadraza");
+            Console.Write("\nLastName: Danmole");
+            Console.Write("1\n101\n1001\n");
+            Console.Write(DateTime.Now);
+            Console.WriteLine(Math.Sqrt(12345));
+            Console.WriteLine("Input your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age + 10);
+        }
+    }
+}
